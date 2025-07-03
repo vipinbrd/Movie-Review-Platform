69a014b09142fe55893d3b2c684b9df011551483
Vipinbrd <vipinbarode7@gmail.com>
2025-07-03 17:10:34 +0530

    added core dependencies for Spring Boot, JWT, MySQL, Lombok, and Swagger

---
+++ movie_review_platform/src/main/java/com/moviereview/MovieReviewPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.moviereview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieReviewPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieReviewPlatformApplication.class, args);
+	}
+
+}
